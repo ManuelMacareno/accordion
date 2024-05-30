54a3c55b8c8eaa626f793a7bae4d84e78d3f4a9a
manu.macareno <manu.macareno@gmail.com>
2024-05-30 14:35:04 +0000

    first commit

---
+++ src/components/accordion/data.js
@@ -0,0 +1,24 @@
+const data = [
+    {
+        id: '1',
+        question: "What are accordion components?",
+        answer: "Accordion components are a type of UI element that allows users to expand and collapse sections of content."
+    },
+    {
+        id: '2',
+        question: "What are they used for?",
+        answer: "They are often used to present information in a compact and organized way"
+    },
+    {
+        id: '3',
+        question: "Accordion as a musical instrument",
+        answer: "Accordion is a musical instrument that allows users to expand and collapse sections of content."
+    },
+    {
+        id: '4',
+        question: "Can i create accordion with a different framework?",
+        answer: "Yes, you can create accordion with a different framework such as React Native."
+    }
+]
+
+export default data;
